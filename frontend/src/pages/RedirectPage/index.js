08160eab841d0c3833ca1a331d290560e008a306
unknown <fernandoarag@gmail.com>
2020-11-26 10:32:48 -0300

    Inserção frontend

---
+++ frontend/src/pages/RedirectPage/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class RedirectPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <p>RedirectPage</p>
+    )
+  }
+}
+
+export default RedirectPage;
